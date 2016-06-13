eb6dbb7c9b9e943295376a33fc63833be9dc2ab5
tochman <thomas@craftacademy.se>
2016-06-13 08:44:52 +0200

    deletes redundant files and updates team info

--- lib/image_helper.rb
+++ lib/image_helper.rb
@@ -1,7 +1,12 @@
 module ImageHelper
   CDN_URL = 'https://assets.craftacademy.se'
 
-  def image_path(path)
-    "#{CDN_URL}/images/#{path}"
+  def image_path(image)
+    "#{CDN_URL}/images/#{image}"
   end
+
+  def avatar_path(image)
+    "avatars/#{image}"
+  end
+
 end
